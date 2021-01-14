7a2420931d8c0bc1e96b35d3270fe4d1c0196e22
Bagus-NT <bagus.nat@gmail.com>
2021-01-15 05:47:34 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenSection />
+    <InstaShayna />
+    <PartnerLogo />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenSection from "@/components/WomenSection.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenSection,
+    InstaShayna,
+    PartnerLogo,
+    FooterShayna,
+  },
+};
+</script>
